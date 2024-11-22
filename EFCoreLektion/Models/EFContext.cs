e64516a29a5d6e756452921aa4e311478f797e6d
Claes Engelin <claes.engelin@zocom.se>
2024-11-22 10:11:15 +0100

    Add project files.

---
+++ EFCoreLektion/Models/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System.Runtime;
+
+namespace EFCoreLektion.Models;
+public class EFContext : DbContext
+{
+    private const string connectionString = 
+        "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EFCoreLektionDemoDB;Integrated Security=True;";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
---
+++ EFCoreLektion/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EFCoreLektion.Models;
+
+Console.WriteLine("Hello, World!");
+
+InsertProduct();
+
+Console.Write("Tryck på en tangent för att stänga fönstret ...");
+Console.ReadKey();
+
+
+void InsertProduct()
+{
+	using (var db = new EFContext())
+	{
+		Product product = new Product();
+		product.Name = "Pen Drive";
+		db.Add(product);
+
+		product = new Product() { Name = "Memory Card"};
+		db.Add(product);
+
+		db.SaveChanges();
+	}
+	return;
+}
+
